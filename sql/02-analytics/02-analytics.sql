a9c192f85f2e93063d928728d45a9e62424d8c49
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-04-30 19:20:05 +0100

    feat: aggregation query by week

--- sql/02-analytics/02-analytics.sql
+++ sql/02-analytics/02-analytics.sql
@@ -4,6 +4,12 @@
  * Task: Exploratory Data Analysis
  */
 
-SELECT count(*) FROM youtube;
+SELECT count(*) FROM lakehouse.youtube;
 
-SELECT avg(likes) AS avg_likes FROM youtube;
+SELECT avg(likes) AS avg_likes FROM lakehouse.youtube;
+
+SELECT
+    date_trunc('week', timestamp)::date AS week,
+    avg(likes)
+FROM lakehouse.youtube
+GROUP BY week;
